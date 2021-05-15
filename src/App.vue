a5a8002882c9e9ece3ee6f33134f7a54f4b971ad
Simo-C3 <shimomae.hitoshi210@mail.kyutech.jp>
2021-05-15 23:17:32 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <v-app id="inspire">
+      <v-app id="inspire">
+        <v-container>
+          <v-bottom-navigation
+            :value="value"
+            color="teal"
+            grow
+          >
+            <v-btn>
+              <span>Recents</span>
+
+              <v-icon>mdi-history</v-icon>
+            </v-btn>
+
+            <v-btn>
+              <span>Favorites</span>
+
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+
+            <v-btn>
+              <span>Nearby</span>
+
+              <v-icon>mdi-map-marker</v-icon>
+            </v-btn>
+          </v-bottom-navigation>
+        </v-container>
+    
+        <v-main>
+          <component :is="currentView" style="transform: translate(10%, 10%);"></component>
+        </v-main>
+    
+        <v-footer app>
+          <span>&copy; {{ new Date().getFullYear() }}</span>
+        </v-footer>
+      </v-app>
+    </v-app>
+  </div>
+</template>
+
+<script>
+import Home from './components/HelloWorld';
+import About from './components/About';
+import vuetify from './plugins/vuetify'
+export default {
+  name: 'App',
+  vuetify,
+  components: {
+    Home,
+    About
+  },
+  data: () => ({
+    currentView: 'Home' 
+  }),
+  methods: {
+    view1() {
+      this.currentView = "Home"
+    },
+    view2() {
+      this.currentView = "About"
+    }
+  }
+}
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-container class="d-flex justifiy-content-center">
+      <v-sheet
+        color="white"
+        elevation="1"
+        height="1000px"
+        width="80%"
+      >
+        <div>
+          <h1 style="text-align: center;">みんなバイク乗ろうよ！！</h1>
+        </div>
+        <img src="../assets/ogp.jpg" style="border-radius: 30px; margin-left: 50px"/>
+      </v-sheet>
+    </v-container>
+  </div>
+</template>
